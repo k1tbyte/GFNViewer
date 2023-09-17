a58889ba5898ce615ccfb6c304522b6cc1973ccb
k1tbyte <steelseries715@gmail.com>
2023-09-17 22:21:40 +0300

    Добавьте файлы проекта.

---
+++ GFNViewer/GFNWrapper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GFNViewer;
+internal sealed partial class GFNWrapper : IDisposable
+{
+    private readonly string LogPath;
+    private readonly System.Timers.Timer Timer;
+    private const int LastLines = 20;
+    private readonly Action<string?> QueueCallback; 
+    private string? LastQueue = "";
+
+    public void Dispose()
+    {
+        Timer.Dispose();
+    }
+
+    public GFNWrapper(Action<string?> callback)
+    {
+        LogPath       = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\NVIDIA Corporation\\GeForceNOW\\debug.log";
+        QueueCallback = callback;
+
+        if (!File.Exists(LogPath))
+            throw new InvalidOperationException("GeForce NOW not found");
+
+        Timer = new(TimeSpan.FromSeconds(10));
+        Timer.Elapsed += DebugLogCheckEvent;
+        Timer.Start();
+    }
+
+    private void DebugLogCheckEvent(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        using var file = File.Open(LogPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        file.Position = file.Length -2;
+
+        int lines = 0;
+        var builder = new StringBuilder();
+        while(file.Position > 0 && lines != LastLines)
+        {
+            var chunk =  file.ReadByte();
+            builder.Insert(0,(char)chunk);
+            if(chunk == '\n')
+            {
+                lines++;
+
+                if (lines == LastLines)
+                    break;
+            }
+            file.Position -= 2;
+        }
+        var match = QueueRegex().Matches(builder.ToString()).LastOrDefault();
+
+        var val = match?.Groups[4]?.Value;
+
+        if (val == LastQueue)
+            return;
+
+        QueueCallback?.Invoke(LastQueue = val);
+    }
+
+    [GeneratedRegex("\\[(.+)/[ ]*(.+):INFO:.+\\].+\\(state: (.*), queue: (.*), eta: (.*)\\)")]
+    private static partial Regex QueueRegex();
+}
---
+++ GFNViewer/Program.cs
@@ -0,0 +1,191 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace GFNViewer;
+
+public sealed class Config
+{
+    public string TelegramToken { get; set; }     = null!;
+    public string AdminUsername { get; set; } = null!;
+    public HashSet<string> AvailableViewers { get; set; } = new();
+}
+
+internal static class Program
+{
+    internal static Config Config { get; private set; } = null!;
+    internal static TelegramBotClient Client { get; private set; } = null!;
+
+    private static GFNWrapper? Wrapper;
+    private static readonly Dictionary<string, (long ChatId, int QueueMessageId)> Viewers = new();
+
+    private static void Init()
+    {
+        bool rewrite = false;
+
+        if (System.IO.File.Exists("appconfig.json"))
+        {
+            Config = JsonConvert.DeserializeObject<Config>(System.IO.File.ReadAllText("appconfig.json"))!;
+        }
+
+        Config ??= new();
+
+        if (string.IsNullOrEmpty(Config.TelegramToken))
+        {
+            Console.Write("Enter telegram bot token: ");
+            Config.TelegramToken = Console.ReadLine();
+            rewrite = true;
+        }
+
+        if(string.IsNullOrEmpty(Config.AdminUsername))
+        {
+            Console.Write("Enter admin username (without @): ");
+            Config.AdminUsername = Console.ReadLine();
+            rewrite = true;
+        }
+
+        if(rewrite)
+        {
+            System.IO.File.WriteAllText("appconfig.json", JsonConvert.SerializeObject(Config));
+        }
+
+    }
+
+    private static void Main(string[] args)
+    {
+
+        var process = System.Diagnostics.Process.GetProcessesByName("GeForceNOW").FirstOrDefault(o => o.MainWindowHandle != IntPtr.Zero);
+
+        Init();
+        Client = new TelegramBotClient(Config.TelegramToken!);
+        Client.StartReceiving(TelegramUpdateHandler, TelegramErrorHandler);
+        Console.ReadLine();
+
+        /*        Rect windowRect = new();
+                GetWindowRect(process.MainWindowHandle, ref windowRect);
+                Console.WriteLine("Width:" + (windowRect.Right - windowRect.Left)); //padding 40
+                Console.WriteLine("Height:" + (windowRect.Bottom - windowRect.Top)); // 130
+
+                SetForegroundWindow(process.MainWindowHandle);
+
+                Bitmap bmpScreenshot = new(350, 70);
+
+                Graphics g = Graphics.FromImage(bmpScreenshot);
+
+                g.CopyFromScreen(windowRect.Left + 40, windowRect.Top + 130, 0, 0, new(350, 70));
+
+                bmpScreenshot.Save("Screenshot.png", ImageFormat.Png);*/
+
+    }
+
+    private static async Task TelegramUpdateHandler(ITelegramBotClient sender, Update e, CancellationToken cancellationToken)
+    {
+        if (e.Message.From.Username != Config.AdminUsername && !Config.AvailableViewers.Contains(e.Message.From.Username.ToLower()))
+            return;
+
+        if(e.Message.Text == "/track" && e.Message.From.Username != Config.AdminUsername)
+        {
+            if(Wrapper == null)
+            {
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "❌ Tracking has not been enabled by an administrator");
+                return;
+            }
+
+            if(!Viewers.TryAdd(e.Message.From.Username,(e.Message.Chat.Id,e.Message.MessageId + 2 )))
+            {
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "❗️ Queue notification is already enabled");
+                return;
+            }
+
+            await Client.SendTextMessageAsync(e.Message.Chat.Id, "✅ Queue notification enabled");
+            await Client.SendTextMessageAsync(e.Message.Chat.Id, "🌀 Waiting for queue information");
+            return;
+        }
+
+        if (e.Message.From.Username != Config.AdminUsername)
+            return;
+
+        if (e.Message.Text == "/start")
+        {
+            if (Wrapper != null)
+            {
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "❗️ Queue tracking is already underway");
+                return;
+            }
+
+            try
+            {
+                Wrapper = new(QueueCallback);
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "✅ Queue tracking started");
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "🌀 Waiting for queue information");
+                Viewers.Add(e.Message.From.Username, (e.Message.Chat.Id, e.Message.MessageId + 2));
+            }
+            catch
+            {
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "❌ Unable to set tracking");
+            }
+            return;
+        }
+
+        if(e.Message.Text == "/stop")
+        {
+            if(Wrapper == null)
+            {
+                await Client.SendTextMessageAsync(e.Message.Chat.Id, "❗️ Queue tracking has not yet been started");
+                return;
+            }
+
+            Wrapper.Dispose();
+            Wrapper = null;
+            await Client.SendTextMessageAsync(e.Message.Chat.Id, "✅ Queue tracking stopped");
+            await Client.DeleteMessageAsync(e.Message.Chat.Id, Viewers[Config.AdminUsername].QueueMessageId);
+
+            foreach (var item in Viewers)
+            {
+                if(item.Key != Config.AdminUsername)
+                {
+                    await Client.SendTextMessageAsync(item.Value.ChatId, "❗️ The administrator has disabled queue tracking");
+                }
+            }
+
+            Viewers.Clear();
+        }
+    }
+
+    private static async void QueueCallback(string? value)
+    {
+        var msgText = value == null ? "🔸 The queue has passed, entering the game" : $"🔹 Queue position: {value}";
+
+
+        foreach (var item in Viewers)
+        {
+            if (value == null)
+                await Client.SendTextMessageAsync(item.Value.ChatId, msgText);
+            else
+            {
+
+                await Client.EditMessageTextAsync(item.Value.ChatId, item.Value.QueueMessageId, msgText);
+            }
+        }
+
+        if (value == null)
+        {
+            Viewers.Clear();
+            Wrapper.Dispose();
+            Wrapper = null;
+        }
+
+        return;
+
+    }
+
+    private async static Task TelegramErrorHandler(ITelegramBotClient sender, Exception e, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"\n{DateTime.Now} | ERROR | " + e);
+        await Client.SendTextMessageAsync(Viewers[Config.AdminUsername].ChatId,"❌ An error has occurred");
+    }
+}
